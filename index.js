783139c897a0c097db0ff3ffa8f9778bc0202306
bhansarikesh <bhansarikesh08@gmail.com>
2020-11-03 19:41:15 +0900

    display translated value

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const baseUrl = 'https://hhxgkiy54l.execute-api.ap-southeast-2.amazonaws.com/stg/translations';
 document.getElementById('btn-translateToEnglish').addEventListener('click', (e) => {
   const newariWord = document.getElementById('newariInput').value;
-  console.log(newariWord);
   if (newariWord && newariWord.trim().length) {
-    const postData = { text: newariWord.trim() };
-    fetch(baseUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(postData),
-    })
-    .then(response => response.json())
+    const word = newariWord.trim();
+    const to = 'english';
+    const url = `${baseUrl}?newariword=${word}`;
+    fetch(url)
+    .then(response => response.text())
     .then(data => {
-      console.log('Success:', data);
-      // document.getElementById('englishInput').value = data;
+      document.getElementById('englishInput').value = data;
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -24,17 +18,14 @@ document.getElementById('btn-translateToEnglish').addEventListener('click', (e) 
 
 document.getElementById("btn-translateToNewari").addEventListener("click", (e) => {
   const englishWord = document.getElementById("englishInput").value;
-  console.log(englishWord);
-  // BE needs an identifier for translation from and to
   if (englishWord && englishWord.trim().length) {
     const word = englishWord.trim();
     const to = 'newari';
     const url = `${baseUrl}?engword=${word}`;
     fetch(url)
-    .then(response => response.json())
+    .then(response => response.text())
     .then(data => {
-      console.log('Success:', data);
-      // document.getElementById('newariInput').value = data;
+      document.getElementById('newariInput').value = data;
     })
     .catch((error) => {
       console.error('Error:', error);
